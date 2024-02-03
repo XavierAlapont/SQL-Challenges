460e9f1ee8d0ad4a50e903f3de7663f44e970104
Xavier <xavier.alapont.0704@gmail.com>
2024-02-03 12:15:11 -0600

    Create Problem 5

---
+++ MS SQL - Challenges/5 - PROBLEM - BASIC ORDERING - ALIASING - EXECUTION ORDER.sql
@@ -0,0 +1,28 @@
+-- PROBLEM 5 
+--------------
+-- BASIC ORDERING - EXECUTION ORDER - ALIASES   
+------------------------- 
+-- PROBLEM STATEMENT - 
+-------------------------
+-- USING THE Sales.vSalesPerson TABLE FROM THE MS ADVENTUREWORKS22 DATABASE
+-- FIND THE LAST NAMES AND NAMES OF THE SALES PEOPLE WITH MORE THAN $200,000 
+-- IN TOTAL SALES AND ORDER THE LIST ACCORDING TO THE SALES QUOTA (IN DESCENDING ORDER) 
+-- AND THEN THE LAST NAME. USE ALIASES OR THE COLUMN POSITIONING ORDER WHEN POSSIBLE.
+
+-- Tested - Xavier Alapont - MS-SQL-SMS 19.2 (56.2)
+
+-- The Aliases defined in Select do not work with the WHERE clause
+-- because the SQL execution order vs. SQL writing order 
+-- WRITING ORDER  : SELECT -> FROM -> WHERE -> GROUP BY -> HAVING -> ORDER BY
+-- EXECUTION ORDER: FROM -> WHERE -> GROUP BY -> HAVING -> SELECT -> ORDER BY
+-- THE WHERE clause is executed before the SELECT clause but the ORDER BY
+-- is executed after the SELECT clause therefore ORDER BY can understand 
+-- Aliases defined in SELECT
+   
+
+SELECT SalesQuota   AS 'Sales Quota',
+	   LastName     AS 'Sales Person Last Name',
+	   FirstName    AS 'Sales Person First Name'
+FROM Sales.vSalesPerson
+WHERE SalesQuota > 200000
+ORDER BY 1 DESC, [Sales Person Last Name] ASC
