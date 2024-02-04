59b9ee57046ecf1705d111d67d9b16a03d533a6b
Xavier <xavier.alapont.0704@gmail.com>
2024-02-03 19:47:59 -0600

    Problem 7

--- MS SQL - Challenges/6 - PROBLEM - BASIC INNER JOIN.sql
+++ MS SQL - Challenges/6 - PROBLEM - BASIC INNER JOIN.sql
@@ -6,15 +6,16 @@
 ------------------------- 
 -- PROBLEM STATEMENT - 
 -------------------------
--- JOINING THE Production.Product TABLE with the Production.ProductSubcategory TABLE 
--- USING THE foreign keys that identify ProductSubcategoryIDs on both tables 
--- THEN using Table Aliases output The product name, Product Number and Product Subcategory Name
+-- Join the Production.Product TABLE with the Production.ProductSubcategory TABLE 
+-- using the foreign keys that identify ProductSubcategoryIDs on both tables then
+-- using Table Aliases output The product name, Product Number and Product Subcategory Name
 
 -- Comment:
--- The table Production.Product returns	504	rows but this Inner Join query returns only 295 rows.
--- A row is	returned only if the value in the joining column from one table	matches	at least one value	
--- of the joining column in	another table. You will	notice a NULL value	in many of the rows for this column,		
--- which means that there is no value with which to join the row to in the Production.ProductSubcategory table.
+-- The table Production.Product returns	504 rows but this Inner Join query returns only 295 rows.
+-- A row is returned only if the value in the joining column (foreign key) from one table matches	
+-- at least one value of the joining column in	another table. You will	notice a NULL value	in 
+-- many of the rows for this column, which means that there is no value 
+-- with which to join the row to in the Production.ProductSubcategory table.
 
 SELECT P.Name, P.ProductNumber, PS.Name AS ProductSubCategoryName
 FROM Production.Product P
