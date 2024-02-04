9a716756ed50258fba0583df54fd53e078661f37
Xavier <xavier.alapont.0704@gmail.com>
2024-02-03 19:50:22 -0600

    Update 7 - PROBLEM - BASIC OUTER JOIN.sql

--- MS SQL - Challenges/7 - PROBLEM - BASIC OUTER JOIN.sql
+++ MS SQL - Challenges/7 - PROBLEM - BASIC OUTER JOIN.sql
@@ -9,10 +9,10 @@
 -- Using the Sales Order Header, Sales Person and Sales Territory Tables
 -- (Sales.SalesOrderHeader, Sales.SalesPerson and Sales.SalesTerritory)
 -- Output the First Name and Last Name of the Sales Person, the Sale Territory,
--- the Sale Order Number and the Sale Total Due for each order from	the Sales.SalesOrderHeader
--- table that has a sales person listed	on the sale. We	also want to view the sales territory 
--- that each sales	person	is	associated	with	regardless	of	whether	or	
--- not they have a listed sales territory.	
+-- the Sale Order Number and the Sale Total Due for each order 
+-- from	the Sales.SalesOrderHeader table that has a sales person listed	on the sale. 
+-- We also want to view the sales territory that each sales person is associated
+-- with regardless of whether or not they have a listed sales territory.	
 -- Return only those rows where the territory’s name is “Northeast” and
 -- sort the output in the TotalDue column in descending	order
 
