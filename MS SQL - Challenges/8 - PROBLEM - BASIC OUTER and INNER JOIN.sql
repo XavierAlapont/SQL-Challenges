b3762387ad9b268b0b3c96eac4267ae5b7f5fa4d
Xavier <xavier.alapont.0704@gmail.com>
2024-02-03 21:08:39 -0600

    Update 8 - PROBLEM - BASIC OUTER and INNER JOIN.sql

--- MS SQL - Challenges/8 - PROBLEM - BASIC OUTER and INNER JOIN.sql
+++ MS SQL - Challenges/8 - PROBLEM - BASIC OUTER and INNER JOIN.sql
@@ -12,6 +12,7 @@
 -- to a territory. Also, return the Name column	from Sales.SalesTerritory. Give this column the column	
 -- alias “Territory Name”. Join to the Person.Person table to return the sales person’s first name and last name.
 -- Now, only include those rows where the territory’s name is either “Northeast” or “Central”
+
 SELECT P.FirstName, P.LastName,
        SP.BusinessEntityID,
        SP.SalesYTD,
