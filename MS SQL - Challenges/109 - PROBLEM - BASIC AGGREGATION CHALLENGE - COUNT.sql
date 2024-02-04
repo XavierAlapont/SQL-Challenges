fb9f804773165668528d0a312d8f17ad75a9b27f
Xavier <xavier.alapont.0704@gmail.com>
2024-02-03 23:31:55 -0600

    Create 109 - PROBLEM - BASIC AGGREGATION CHALLENGE - COUNT.sql

---
+++ MS SQL - Challenges/109 - PROBLEM - BASIC AGGREGATION CHALLENGE - COUNT.sql
@@ -0,0 +1,22 @@
+-- PROBLEM 9  
+--
+-- Tested - Xavier Alapont - MS-SQL-SMS 19.2 (56.2)
+--------------
+-- BASIC AGGREGATE CHALLENGE - COUNT 
+------------------------- 
+-- PROBLEM STATEMENT - 
+-------------------------
+
+-- Join the Production.Product table and the Production.ProductInventory table for	
+-- only the	products that appear in both tables. Use the ProductID as the joining column.	
+-- Count the number of products in the Join, the number of distinct products and the
+-- number of products with a listed price not zero and cheaper than $10. 
+
+
+SELECT COUNT(Name) AS 'Number of Products', 
+       COUNT(DISTINCT Name) AS 'Number of Distinct Products',
+	   COUNT(CASE WHEN P.ListPrice < 10 THEN Name END) AS 'Number of Cheap Products'
+FROM Production.Product AS P
+INNER JOIN Production.ProductInventory I
+ON I.ProductID = P.ProductID
+WHERE P.ListPrice > 0
