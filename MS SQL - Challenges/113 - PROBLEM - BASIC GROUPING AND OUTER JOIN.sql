efa900f9cac82758b7a1091af764a6043b9f0453
Xavier <xavier.alapont.0704@gmail.com>
2024-02-04 18:32:36 -0600

    Update 113 - PROBLEM - BASIC GROUPING AND OUTER JOIN.sql

--- MS SQL - Challenges/113 - PROBLEM - BASIC GROUPING AND OUTER JOIN.sql
+++ MS SQL - Challenges/113 - PROBLEM - BASIC GROUPING AND OUTER JOIN.sql
@@ -1,33 +1,24 @@
--- PROBLEM 12  
+-- PROBLEM 13  
 --
 -- Tested - Xavier Alapont - MS-SQL-SMS 19.2 (56.2)
 --------------
--- BASIC GROUPING WITH JOINS CHALLENGE 
+-- BASIC GROUPING WITH OUTER JOINS CHALLENGE 
 ------------------------- 
 -- PROBLEM STATEMENT - 
 -------------------------
--- Using the Sales Order Header, Sales Person and Sales Territory Tables
--- (Sales.SalesOrderHeader, Sales.SalesPerson and Sales.SalesTerritory)
--- Determine the Total Sales by Person in the year 2013 
--- (SUM TotalDue in the SalesOrderHeader Table) and
--- output the First Name and Last Name of the Sales Person 
--- (from the Person.Person Table) and the Sale Territory Name 
--- (from the SalesTerritory table). Do not display results with 
--- NULL values for the territory name or the sales person name column.
--- Sort the output in the Territory Name and Total Sales in descending order
 
+-- Using the Sales.SalesOrderHeader table determine	
+-- how many sales occurred in each territory between	
+-- July	1, 2010	and	December 31, 2013 ?		
+-- Order the results by	the sale count in descending order.
+-- group the results not by	the	TerritoryID	but	by	
+-- the name of the territory (found in the Sales.SalesTerritory table).
 
-SELECT  P.FirstName AS [SalesPerson First Name],		
-        P.LastName AS [SalesPerson Last Name],  
-		T.Name AS TerritoryName, 
-		SUM(TotalDue) AS [Total 2013 Sales]
+SELECT
+ST.Name AS TerritoryName,
+COUNT(*) AS SalesCount
 FROM Sales.SalesOrderHeader SOH
-INNER JOIN Sales.SalesPerson SP
-ON SP.BusinessEntityID = SOH.SalesPersonID
-INNER JOIN Person.Person P
-ON P.BusinessEntityID = SP.BusinessEntityID
-INNER JOIN Sales.SalesTerritory T
-ON T.TerritoryID = SP.TerritoryID
-WHERE OrderDate BETWEEN '1/1/2013' AND '12/31/2013'
-GROUP BY T.Name, P.FirstName, P.LastName
-ORDER BY 3, 4 DESC
+LEFT OUTER JOIN Sales.SalesTerritory ST
+ON ST.TerritoryID = SOH.TerritoryID
+WHERE OrderDate BETWEEN '7/1/2010' AND '12/31/2013'
+GROUP BY ST.Name
